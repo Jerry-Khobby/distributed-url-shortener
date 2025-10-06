859c0bc788162ed7464fc20d87366b35d766ca52
Jerry-Khobby <jerrymardeburg@gmail.com>
2025-10-06 01:13:56 -1200

    trying to connect to the database supabase

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt';
+import { JwtAuthGuard } from './guards/jwt.auth.guard';
+import { SupabaseStrategy } from './strategies/supabase.strategy';
+import { config } from 'process';
+@Module({
+  imports:[
+    PassportModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      useFactory:(configService:ConfigService)=>{
+        return{
+          global:true,
+          secret:configService.get<string>('SUPABASE_JWT_SECRET'),
+          signOptions:{expiresIn:3600},
+        }
+      },
+      inject:[ConfigService]
+    })
+  ],
+    providers: [JwtAuthGuard, SupabaseStrategy],
+    exports: [JwtAuthGuard, JwtModule]
+})
+
+export class AuthModule {}
