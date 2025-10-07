f2954219a00ba2b771f9b37f3813de7a34834cfa
Jerry-Khobby <jerrymardeburg@gmail.com>
2025-10-06 23:25:39 -1200

    added caching and redis store to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UrlsModule } from './urls/urls.module';
 import { ConfigModule } from '@nestjs/config';
-import { SupabaseModule } from './auth/strategies/supabase.strategy';
+import { SupabaseModule } from './supabase/supabase.strategy';
+import { CacheModule } from '@nestjs/cache-manager';
+import { RedisOptions } from './config/app-options.constants';
 @Module({
   imports: [AuthModule, UrlsModule,ConfigModule.forRoot({isGlobal:true}),
 SupabaseModule,
+CacheModule.registerAsync(RedisOptions)
   ],
   controllers: [AppController],
   providers: [AppService],
